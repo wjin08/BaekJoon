2c5907c0b94bcd8681ba8f19c534f5b8026fbf9e
wjin08 <54980660+wjin08@users.noreply.github.com>
2025-04-14 00:07:40 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	scanf("%d", &a);
+
+	int f = 0;
+	f = a / 4;
+
+	for (int i = 1;i <= f;i++) {
+		printf("long ");
+	
+	}
+
+	printf("int");
+
+
+	return 0;
+}
